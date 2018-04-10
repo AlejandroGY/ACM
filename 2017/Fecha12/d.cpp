07a88e66b409dd0bed8a84f0e8585c083a14bf32
AlejandroGY <alejandro.gy96@gmail.com>
2018-04-09 21:35:30 -0500

    ACM

--- 2017/Fecha12/d.cpp
+++ 2017/Fecha12/d.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <cstring>
 using namespace std;
 
 unsigned long long int solve(unsigned long long int n) {
@@ -20,28 +17,7 @@ int main() {
 	ios_base::sync_with_stdio(0);
 
 	unsigned long long int n;
-	//cin >> num;
-	//int n = stoi(num);
 	while(cin >> n, n) {
-		//cout << num[1] -'0';
 		cout << solve(n) << "\n";
 	}
-/*
-cout << num.size();
-	
-	while(num != "0") {
-		while(num.size() > 1) {
-			string res = "";
-			for(int i = 0; i < num.size(); i++) {
-				res += num[i]-'0';
-				cout << res << '\n';
-			}
-			num;
-			num = res;
-			cout << num << " : num\n";
-		}
-		cout << num << "\n";
-		cin >> num;
-	}
-*/
 }
